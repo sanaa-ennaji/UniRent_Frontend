84381b951f985bd1780cf342fc51a570234c012d
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-03-11 10:05:14 +0000

    modefying the front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
-    { path: '', component: HomepageComponent }
+    { path: '', component: HomepageComponent },
+    {path: 'login', component: LoginComponent},
+    {path: 'register',component: RegisterComponent }
 ];
