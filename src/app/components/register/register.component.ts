cde28161fe7e1782c33c4686029e94aa06474e2e
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-03-12 12:18:06 +0000

    modefying the sthyle

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUserGraduate, faBuilding } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, FontAwesomeModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
   selectedRole: 'student' | 'landlord' = 'student'; 
 
+
+  faStudent = faUserGraduate;
+  faLandlord = faBuilding;
+
   toggleRole(role: 'student' | 'landlord') {
     this.selectedRole = role;
   }
 }
+
