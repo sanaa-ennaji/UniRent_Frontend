0a21183990fff2635f78905f7a74074c26d987c0
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-03-13 19:56:07 +0000

    install leaflet map

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,14 @@
-import { Component, NgModule } from '@angular/core';
-import { FormBuilder, FormGroup, NgModel, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component} from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 import { LoginRequestDTO } from '../../models/login-request.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true ,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule , CommonModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
---
+++ src/app/university-map/university-map.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-university-map',
+  imports: [],
+  templateUrl: './university-map.component.html',
+  styleUrl: './university-map.component.css'
+})
+export class UniversityMapComponent {
+
+}
