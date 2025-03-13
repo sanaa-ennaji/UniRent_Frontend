486eea1ad95c3eeadc695ed89da0dc782ae6d234
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-03-13 10:28:36 +0000

    add serviceauth and login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, NgModule } from '@angular/core';
+import { FormBuilder, FormGroup, NgModel, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from '../../core/services/user.service';
+import { AuthService } from '../../core/services/auth.service';
+import { LoginRequestDTO } from '../../models/login-request.model';
 
 @Component({
   selector: 'app-login',
+  standalone: true ,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
@@ -29,7 +32,7 @@ export class LoginComponent {
       this.authService.login(credentials).subscribe({
         next: (response) => {
           console.log('Login successful:', response);
-          this.router.navigate(['/dashboard']); // Redirect to dashboard or home page
+          this.router.navigate(['/register']); 
         },
         error: (error) => {
           console.error('Login failed:', error);
---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { LoginRequestDTO } from '../../models/login-request.model';
+
+export interface AuthenticationResponse {
+  jwt: string;
+  email: string;
+  roleName: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:8888/api/authenticate'; 
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(credentials: LoginRequestDTO): Observable<AuthenticationResponse> {
+    return this.http.post<AuthenticationResponse>(this.apiUrl, credentials).pipe(
+      tap((response) => {
+        localStorage.setItem('jwt', response.jwt);
+        localStorage.setItem('roleName', response.roleName);
+      })
+    );
+  }
+
+  logout(): void {
+    localStorage.removeItem('jwt');
+    localStorage.removeItem('roleName');
+    this.router.navigate(['/login']);
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('jwt');
+  }
+
+  getJwt(): string | null {
+    return localStorage.getItem('jwt');
+  }
+
+  getRoleName(): string | null {
+    return localStorage.getItem('roleName');
+  }
+}
