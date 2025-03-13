732ee6ba87643e2cb2b10fb7e54f15128c28dbfb
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-03-13 10:20:35 +0000

    add rrequest login model

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/user.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  errorMessage: string | null = null;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const credentials: LoginRequestDTO  = this.loginForm.value;
+      this.authService.login(credentials).subscribe({
+        next: (response) => {
+          console.log('Login successful:', response);
+          this.router.navigate(['/dashboard']); // Redirect to dashboard or home page
+        },
+        error: (error) => {
+          console.error('Login failed:', error);
+          this.errorMessage = 'Invalid email or password. Please try again.';
+        },
+      });
+    } else {
+      this.errorMessage = 'Please fill out the form correctly.';
+    }
+  }
 }
