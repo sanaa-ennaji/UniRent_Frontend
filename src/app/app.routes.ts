11c34ff9d0ca66ece3166ff4ef628643e2610f92
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-03-14 11:46:07 +0000

    add sedebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { MapComponent } from './components/map/map.component';
 import { CreatePropertyComponent } from './components/landLord/create-property/create-property.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 export const routes: Routes = [
     { path: '', component: HomepageComponent },
     {path: 'login', component: LoginComponent},
     {path: 'register',component: RegisterComponent },
-    {path: 'landlord/property/create' , component: CreatePropertyComponent}
+    {path: 'landlord/property/create' , component: CreatePropertyComponent},
+    {path: 'sidebar' , component : SidebarComponent}
 ];
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUserGraduate, faBuilding } from '@fortawesome/free-solid-svg-icons';
+import { NavbarComponent } from "../navbar/navbar.component";
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [FontAwesomeModule, NavbarComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-
+  property = faBuilding;
 }
