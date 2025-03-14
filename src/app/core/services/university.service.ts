acca668a3b53580d46d3907d0a4ae86e3c731792
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-03-14 00:57:16 +0000

    add service for uni

--- src/app/core/services/university.service.ts
+++ src/app/core/services/university.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { University } from '../../models/';
+import { University } from '../../models/university.model';
 
 @Injectable({
   providedIn: 'root',
