6b9c243ac31eee0603da9961fe6c3d6d2d45f0ff
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-03-14 01:01:19 +0000

    add map components

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { UniversityService } from '../../core/services/university.service';
+
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -6,7 +9,30 @@ import { Component } from '@angular/core';
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
-export class MapComponent {
-private map : any;
-constructor (private universityService; universityService)
+export class MapComponent implements OnInit {
+  private map: any;
+
+  constructor(private universityService: UniversityService) {}
+
+  ngOnInit(): void {
+    this.initMap();
+    this.loadUniversities();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([31.7917, -7.0926], 6); 
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+  }
+
+  private loadUniversities(): void {
+    this.universityService.getUniversities().subscribe(universities => {
+      universities.forEach(university => {
+        const marker = L.marker([university.latitude, university.longitude]).addTo(this.map);
+        marker.bindPopup(`<b>${university.name}</b><br>${university.city}`);
+      });
+    });
+  }
 }
