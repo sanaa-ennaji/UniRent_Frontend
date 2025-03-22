61a4712d9557f8d70a7839699e67acd814f0e43c
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-03-22 21:46:27 +0000

    ensure landlordId is not null before making the API call

--- src/app/components/landlord-bookings/landlord-bookings.component.ts
+++ src/app/components/landlord-bookings/landlord-bookings.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookingService } from '../../core/services/booking.service';
+import { AuthService } from '../../core/services/auth.service';
+import { BookingResponseDTO } from '../../models/booking.model';
+import { NavbarComponent } from "../../shared/navbar/navbar.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landlord-bookings',
-  imports: [],
+  standalone: true,
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './landlord-bookings.component.html',
-  styleUrl: './landlord-bookings.component.css'
-})
-export class LandlordBookingsComponent {
+  styleUrls: ['./landlord-bookings.component.css']
 
+})
+export class LandlordBookingsComponent implements OnInit {
+  bookings: BookingResponseDTO[] = [];
+  landlordId: number | null = null;
+
+  constructor(
+    private bookingService: BookingService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    // Get the authenticated landlord's ID
+    const user = this.authService.getCurrentUser();
+    if (user) {
+      this.landlordId = user.id;
+  
+
+      if (this.landlordId !== null) {
+        this.bookingService.getConfirmedBookingsByLandlordId(this.landlordId).subscribe(
+          (data: BookingResponseDTO[]) => {
+            this.bookings = data;
+            console.log('Confirmed bookings fetched successfully:', this.bookings);
+          },
+          (error) => {
+            console.error('Error fetching confirmed bookings:', error);
+          }
+        );
+      } else {
+        console.error('Landlord ID is null.');
+      }
+    } else {
+      console.error('User is not logged in.');
+    }
+  }
 }
