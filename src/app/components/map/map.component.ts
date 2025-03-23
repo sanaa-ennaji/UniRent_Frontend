131dfeb8c82eb041ee23aeac004845fa89be4294
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-03-23 02:58:05 +0000

    display the properties in themap

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { UniversityService } from '../../core/services/university.service';
-
+import { PropertyService } from '../../core/services/property.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -9,9 +10,13 @@ import { UniversityService } from '../../core/services/university.service';
 export class MapComponent implements OnInit {
   private map: any;
   private L: any; 
+  @Input() properties: any[] = []; 
 
-  constructor(private universityService: UniversityService) {}
-
+  constructor(
+    private universityService: UniversityService,
+    private propertyService: PropertyService,
+    private router: Router
+  ) {}
   async ngOnInit(): Promise<void> {
     if (typeof window !== 'undefined') { 
       const L = await import('leaflet'); 
@@ -19,6 +24,7 @@ export class MapComponent implements OnInit {
       this.initMap();
       this.loadUniversities();
     }
+    
   }
 
   private initMap(): void {
@@ -52,4 +58,31 @@ export class MapComponent implements OnInit {
     });
   }
   
+  private loadProperties(): void {
+    const iconUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png';
+    const iconRetinaUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png';
+    const shadowUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png';
+
+    const customIcon = this.L.icon({
+      iconUrl: iconUrl,
+      iconRetinaUrl: iconRetinaUrl,
+      shadowUrl: shadowUrl,
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
+
+  
+    this.properties.forEach(property => {
+      const marker = this.L.marker([property.latitude, property.longitude], { icon: customIcon }).addTo(this.map);
+
+  
+      marker.bindPopup(`<b>${property.title}</b><br>Price: $${property.price}`);
+      marker.on('click', () => {
+        this.router.navigate(['/properties', property.id]);
+      });
+    });
+  }
 }
+  
